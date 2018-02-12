4566d571ee41a2a88bd876ac6edd453ea3c5cc0e
WendyKKelly <wendykkelly@gmail.com>
2018-02-12 10:09:39 -0800

    UX

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 var expect = require('expect');
 
 var {generateMessage} = require('./message');
+var {generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -10,5 +11,18 @@ describe('generateMessage', () => {
 
     expect(message).toMatchObject({from, text});
     expect(typeof message.createdAt).toBe('number');
+  });
+});
+describe('generateLocationMessage', () => {
+  it('should generate correct location', () => {
+    var from = 'Me';
+    var latitude = 49.491725699999996;
+    var longitude = -117.2855557;
+    var url = `https://www.google.com/maps?q=${latitude},${longitude}`;
+
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message).toMatchObject({from, url});
+
   });
 });
